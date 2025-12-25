66e1de724adcd0112201cbba86130eb41d8f9553
yum <contact@meyume.com>
2025-12-25 00:27:55 -0500

    feat(wallhaven): improve randomness of wallpaper selection by increasing sample size

--- wally-providers/src/providers/wallhaven.rs
+++ wally-providers/src/providers/wallhaven.rs
@@ -1,6 +1,7 @@
 use anyhow::Context;
 use reqwest::Url;
 use serde::{Deserialize, Serialize};
+use tokio::task::JoinHandle;
 
 use crate::providers::WallpaperProvider;
 
@@ -8,12 +9,29 @@ const WALLHAVEN_API_URL: &str = "https://wallhaven.cc/api/v1/search";
 
 #[derive(Debug, Serialize, Deserialize)]
 struct WallhavenData {
+    id: String,
+    dimension_x: u32,
+    dimension_y: u32,
+    resolution: String,
+    file_size: u32,
+    file_type: String,
     path: Url,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct WallhavenMeta {
+    current_page: u32,
+    last_page: u32,
+    per_page: u32,
+    total: u32,
+    query: Option<String>,
+    seed: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct WallhavenResponse {
     data: Vec<WallhavenData>,
+    meta: WallhavenMeta,
 }
 
 pub struct Wallhaven {}
@@ -24,19 +42,31 @@ impl WallpaperProvider for Wallhaven {
     }
 
     async fn random(&self) -> anyhow::Result<Url> {
-        let response: WallhavenResponse = reqwest::get(WALLHAVEN_API_URL)
-            .await?
-            .json()
-            .await
-            .context("Unable to parse wallhaven response into json")?;
+        let mut handles = Vec::new();
 
-        let selected = rand::random_range(0..response.data.len());
+        // fire off requests for 4 pages each of (usually) 24 wallpapers
+        // for a larger selection of wallpapers to choose from for increased randomness.
+        for page in 1..=4 {
+            let handle: JoinHandle<anyhow::Result<WallhavenResponse>> = tokio::spawn(async move {
+                reqwest::get(format!("{WALLHAVEN_API_URL}?page={page}"))
+                    .await?
+                    .json()
+                    .await
+                    .context("Unable to parse wallhaven response into json")
+            });
+            handles.push(handle);
+        }
 
-        Ok(response
-            .data
+        let mut results = Vec::new();
+        for handle in handles {
+            let response = handle.await.context("Failed to fire request")??;
+            results.extend(response.data.into_iter().map(|data| data.path))
+        }
+        let selected = rand::random_range(0..results.len());
+
+        Ok(results
             .get(selected)
             .context("selected item does not exist")?
-            .path
             .clone())
     }
 }
@@ -50,7 +80,6 @@ mod tests {
     async fn test_get_random_wallpaper() {
         let provider = Wallhaven {};
         let url = provider.random().await;
-        assert!(url.is_ok());
-        dbg!(url.unwrap().to_string());
+        assert!(url.is_ok(), "{:?}", url);
     }
 }
